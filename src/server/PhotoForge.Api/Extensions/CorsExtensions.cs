0187520ccd7b39be129eb3a29d7e468a3e8810fd
Kacper S <kacpersmal@gmail.com>
2024-01-30 22:56:49 +0100

    Add cors

---
+++ src/server/PhotoForge.Api/Extensions/CorsExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace PhotoForge.Api.Extensions;
+
+public static class CorsExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy",
+                builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+        });
+    } 
+}
