4b30ecb01b64754057ab9ed97e809b88b40f220d
Kacper S <kacpersmal@gmail.com>
2024-01-31 19:44:50 +0100

    update tests

--- src/server/PhotoForge.Application.Tests/Features/Users/CreateUserCommandHandlerTests.cs
+++ src/server/PhotoForge.Application.Tests/Features/Users/CreateUserCommandHandlerTests.cs
@@ -16,27 +16,25 @@ public class CreateUserCommandHandlerTests
             .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
             .Options;
 
-        using (var context = new AppDbContext(options))
+        await using var context = new AppDbContext(options);
+        var hashedValueService = new Mock<IHashedValueService>();
+        var handler = new CreateUserCommandHandler(context, hashedValueService.Object);
+
+        var createUserCommand = new CreateUserCommand
         {
-            var hashedValueService = new Mock<IHashedValueService>();
-            var handler = new CreateUserCommandHandler(context, hashedValueService.Object);
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@example.com",
+            Password = "password123"
+        };
 
-            var createUserCommand = new CreateUserCommand
-            {
-                FirstName = "John",
-                LastName = "Doe",
-                Email = "john.doe@example.com",
-                Password = "password123"
-            };
+        // Act
+        await handler.Handle(createUserCommand, CancellationToken.None);
 
-            // Act
-            await handler.Handle(createUserCommand, CancellationToken.None);
-
-            // Assert
-            var user = await context.Users.SingleOrDefaultAsync();
-            user.Should().NotBeNull();
-            user?.Email.Address.Should().Be(createUserCommand.Email);
-        }
+        // Assert
+        var user = await context.Users.SingleOrDefaultAsync();
+        user.Should().NotBeNull();
+        user?.Email.Address.Should().Be(createUserCommand.Email);
     }
 
     [Test]
@@ -47,29 +45,27 @@ public class CreateUserCommandHandlerTests
             .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
             .Options;
 
-        using (var context = new AppDbContext(options))
+        await using var context = new AppDbContext(options);
+        var hashedValueService = new Mock<IHashedValueService>();
+        var handler = new CreateUserCommandHandler(context, hashedValueService.Object);
+
+        var createUserCommand = new CreateUserCommand
         {
-            var hashedValueService = new Mock<IHashedValueService>();
-            var handler = new CreateUserCommandHandler(context, hashedValueService.Object);
+            FirstName = "John",
+            LastName = "Doe",
+            Email = "john.doe@example.com",
+            Password = "password123"
+        };
 
-            var createUserCommand = new CreateUserCommand
-            {
-                FirstName = "John",
-                LastName = "Doe",
-                Email = "john.doe@example.com",
-                Password = "password123"
-            };
+        // Add a user to the in-memory database to simulate an existing user
+        var existingUser = new User(new FullName("Existing", "User"), new EmailAddress(createUserCommand.Email), new HashedValue(new byte[1], new byte[1]));
+        context.Users.Add(existingUser);
+        await context.SaveChangesAsync();
 
-            // Add a user to the in-memory database to simulate an existing user
-            var existingUser = new User(new FullName("Existing", "User"), new EmailAddress(createUserCommand.Email), new HashedValue(new byte[1], new byte[1]));
-            context.Users.Add(existingUser);
-            await context.SaveChangesAsync();
+        // Act
+        Func<Task> act = async () => await handler.Handle(createUserCommand, CancellationToken.None);
 
-            // Act
-            Func<Task> act = async () => await handler.Handle(createUserCommand, CancellationToken.None);
-
-            // Assert
-            await act.Should().ThrowAsync<EntityConflictException>();
-        }
+        // Assert
+        await act.Should().ThrowAsync<EntityConflictException>();
     }
 }
--- src/server/PhotoForge.Application.Tests/Features/Users/GetAllUsersQueryHandlerTests.cs
+++ src/server/PhotoForge.Application.Tests/Features/Users/GetAllUsersQueryHandlerTests.cs
@@ -17,34 +17,32 @@ public class GetAllUsersQueryHandlerTests
             .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
             .Options;
 
-        using (var context = new AppDbContext(options))
+        await using var context = new AppDbContext(options);
+        // Seed the in-memory database with some test data
+        var users = new List<User>
         {
-            // Seed the in-memory database with some test data
-            var users = new List<User>
-            {
-                new(new FullName("John", "Doe"), new EmailAddress("john.doe@example.com"), new HashedValue(new byte[1], new byte[1])),
-                new(new FullName("Jane", "Doe"), new EmailAddress("jane.doe@example.com"), new HashedValue(new byte[1], new byte[1])),
-                new(new FullName("Bob", "Smith"), new EmailAddress("bob.smith@example.com"), new HashedValue(new byte[1], new byte[1]))
-            };
-            context.Users.AddRange(users);
-            await context.SaveChangesAsync();
+            new(new FullName("John", "Doe"), new EmailAddress("john.doe@example.com"), new HashedValue(new byte[1], new byte[1])),
+            new(new FullName("Jane", "Doe"), new EmailAddress("jane.doe@example.com"), new HashedValue(new byte[1], new byte[1])),
+            new(new FullName("Bob", "Smith"), new EmailAddress("bob.smith@example.com"), new HashedValue(new byte[1], new byte[1]))
+        };
+        context.Users.AddRange(users);
+        await context.SaveChangesAsync();
 
-            var mapperConfig = new MapperConfiguration(cfg =>
-            {
-                cfg.AddProfile<UsersProfile>();
-            });
-            var mapper = mapperConfig.CreateMapper();
-            var handler = new GetAllUsersQueryHandler(context, mapper);
-            var query = new GetAllUsersQuery { Page = 0, Items = 2, SearchString = "" };
+        var mapperConfig = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<UsersProfile>();
+        });
+        var mapper = mapperConfig.CreateMapper();
+        var handler = new GetAllUsersQueryHandler(context, mapper);
+        var query = new GetAllUsersQuery { Page = 0, Items = 2, SearchString = "" };
 
-            // Act
-            var result = await handler.Handle(query, CancellationToken.None);
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
 
-            // Assert
-            result.Users.Should().HaveCount(2);
-            result.Users.Should().ContainSingle(user => user.FullName == "John Doe");
-            result.Users.Should().ContainSingle(user => user.FullName == "Jane Doe");
-        }
+        // Assert
+        result.Users.Should().HaveCount(2);
+        result.Users.Should().ContainSingle(user => user.FullName == "John Doe");
+        result.Users.Should().ContainSingle(user => user.FullName == "Jane Doe");
     }
 
     [Test]
@@ -55,32 +53,30 @@ public class GetAllUsersQueryHandlerTests
             .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
             .Options;
 
-        using (var context = new AppDbContext(options))
+        await using var context = new AppDbContext(options);
+        // Seed the in-memory database with some test data
+        var users = new List<User>
         {
-            // Seed the in-memory database with some test data
-            var users = new List<User>
-            {
-                new(new FullName("John", "Doe"), new EmailAddress("john.doe@example.com"), new HashedValue(new byte[1], new byte[1])),
-                new(new FullName("Jane", "Doe"), new EmailAddress("jane.doe@example.com"), new HashedValue(new byte[1], new byte[1])),
-                new(new FullName("Bob", "Smith"), new EmailAddress("bob.smith@example.com"), new HashedValue(new byte[1], new byte[1]))
-            };
-            context.Users.AddRange(users);
-            await context.SaveChangesAsync();
+            new(new FullName("John", "Doe"), new EmailAddress("john.doe@example.com"), new HashedValue(new byte[1], new byte[1])),
+            new(new FullName("Jane", "Doe"), new EmailAddress("jane.doe@example.com"), new HashedValue(new byte[1], new byte[1])),
+            new(new FullName("Bob", "Smith"), new EmailAddress("bob.smith@example.com"), new HashedValue(new byte[1], new byte[1]))
+        };
+        context.Users.AddRange(users);
+        await context.SaveChangesAsync();
 
-            var mapperConfig = new MapperConfiguration(cfg =>
-            {
-                cfg.AddProfile<UsersProfile>();
-            });
-            var mapper = mapperConfig.CreateMapper();
-            var handler = new GetAllUsersQueryHandler(context, mapper);
-            var query = new GetAllUsersQuery { Page = 0, Items = 2, SearchString = "Jane" };
+        var mapperConfig = new MapperConfiguration(cfg =>
+        {
+            cfg.AddProfile<UsersProfile>();
+        });
+        var mapper = mapperConfig.CreateMapper();
+        var handler = new GetAllUsersQueryHandler(context, mapper);
+        var query = new GetAllUsersQuery { Page = 0, Items = 2, SearchString = "Jane" };
 
-            // Act
-            var result = await handler.Handle(query, CancellationToken.None);
+        // Act
+        var result = await handler.Handle(query, CancellationToken.None);
 
-            // Assert
-            result.Users.Should().HaveCount(1);
-            result.Users.Should().ContainSingle(user => user.FullName == "Jane Doe");
-        }
+        // Assert
+        result.Users.Should().HaveCount(1);
+        result.Users.Should().ContainSingle(user => user.FullName == "Jane Doe");
     }
 }
